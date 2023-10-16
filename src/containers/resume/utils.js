6ee0fc33fe4e6bd655544ad0e71ca461ec1a5422
Madhusudhana Rao <136260460+Koya-Madhusudhana-Rao@users.noreply.github.com>
2023-10-16 10:25:52 +0530

    commit

---
+++ src/containers/resume/utils.js
@@ -0,0 +1,42 @@
+export const data = {
+    experience: [
+      {
+        title: "AWS CLOUD PRACTITIONER",
+        subTitle: "AWS, CP",
+        description:
+          " Aws cloud practitioner basics working with aws cloud",
+      },
+      {
+        title: "MERN FULL STACK",
+        subTitle: "MERN",
+        description:
+          "Learned MongoDB ReactJS ExpressJS and NodeJS and DID some project ",
+      },
+      
+    ],
+    education: [
+      {
+        title: "Secondary",
+        subTitle: "GOOD NEWS ENGLIISH MEDIUM HIGH SCHOOL",
+        description:
+          "10th Grade",
+      },
+      {
+        title: "Higher Secondary",
+        subTitle: "NARAYANA JUNIOR COLLEGE",
+        description:
+          "12th Grade in Science",
+
+        
+        
+        
+        
+      },
+      {
+        title: "B.Tech",
+        subTitle: "Vellore institute of technology - AP",
+        description:
+          "Bachelor of technology in computer science specialized in Artificial Inteligence and Machine Learning",
+      },
+    ],
+  };
---
+++ src/containers/skills/utils.js
@@ -0,0 +1,54 @@
+export const skillsData = [
+    {
+      label: "FRONT END",
+      data: [
+        {
+          skillName: "HTML",
+          percentage: "90",
+        },
+        {
+          skillName: "CSS",
+          percentage: "70",
+        },
+        {
+          skillName: "JAVASCRIPT",
+          percentage: "50",
+        },
+        {
+          skillName: "REACTJS",
+          percentage: "50",
+        },
+      ],
+    },
+    {
+      label: "BACK END",
+      data: [
+        {
+          skillName: "JAVA",
+          percentage: "70",
+        },
+        {
+          skillName: "PYTHON",
+          percentage: "65",
+        },
+        {
+          skillName: "ANDROID STUDIO",
+          percentage: "35",
+        },
+      ],
+    },
+    {
+      label: "DATABASES",
+      data: [
+        {
+          skillName: "SQL",
+          percentage: "70",
+        },
+        {
+          skillName: "MONGO DB",
+          percentage: "65",
+        },
+        
+      ],
+    },
+  ];
