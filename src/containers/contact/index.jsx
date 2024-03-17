1de83fa59db76d1e42ae3a060a7cf66d9ef10d91
Madhusudhana Rao <madhusudhanaraokoya@gmail.com>
2024-03-17 17:54:11 +0530

    Made Contact Page responsive

--- src/containers/contact/index.jsx
+++ src/containers/contact/index.jsx
@@ -1,10 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import PageHeaderContent from "../../components/pageHeaderContent";
 import { BsInfoCircleFill } from "react-icons/bs";
 import { Animate } from "react-simple-animate";
+import emailjs from "emailjs-com";
 import "./styles.scss";
 
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    description: ""
+  });
+  const [isSubmitted, setIsSubmitted] = useState(false); // State variable to track submission status
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_q076zv9",
+        "template_hiudcfl",
+        e.target,
+        "OtyldcbYOdHaCwa89"
+      )
+      .then(
+        (result) => {
+          console.log("Email sent successfully:", result.text);
+          // Reset form after successful submission
+          setFormData({
+            name: "",
+            email: "",
+            description: ""
+          });
+          // Set isSubmitted to true after successful submission
+          setIsSubmitted(true);
+        },
+        (error) => {
+          console.error("Failed to send email:", error.text);
+        }
+      );
+  };
+
   return (
     <section id="contact" className="contact">
       <PageHeaderContent
@@ -37,47 +81,60 @@ const Contact = () => {
           }}
         >
           <div className="contact__content__form">
-            <div className="contact__content__form__controlswrapper">
-              <div>
-                <input
-                  required
-                  name="name"
-                  className="inputName"
-                  type={"text"}
-                />
-                <label htmlFor="name" className="nameLabel">
-                  Name
-                </label>
-              </div>
-              <div>
-                <input
-                  required
-                  name="email"
-                  className="inputEmail"
-                  type={"text"}
-                />
-                <label htmlFor="email" className="emailLabel">
-                  Email
-                </label>
-              </div>
-              <div>
-                <textarea
-                  required
-                  name="description"
-                  className="inputDescription"
-                  type={"text"}
-                  rows="5"
-                />
-                <label htmlFor="description" className="descriptionLabel">
-                  Description
-                </label>
-              </div>
-            </div>
-            <button>Submit</button>
+            {isSubmitted ? (
+              <p className="thank-you-message">Thank you for your message! We will get back to you soon.</p>
+            ) : (
+              <form onSubmit={handleSubmit}>
+                <div className="contact__content__form__controlswrapper">
+                  <div>
+                    <input
+                      required
+                      name="name"
+                      className="inputName"
+                      type="text"
+                      value={formData.name}
+                      onChange={handleChange}
+                    />
+                    <label htmlFor="name" className="nameLabel">
+                      Name
+                    </label>
+                  </div>
+                  <div>
+                    <input
+                      required
+                      name="email"
+                      className="inputEmail"
+                      type="email"
+                      value={formData.email}
+                      onChange={handleChange}
+                    />
+                    <label htmlFor="email" className="emailLabel">
+                      Email
+                    </label>
+                  </div>
+                  <div>
+                    <textarea
+                      required
+                      name="description"
+                      className="inputDescription"
+                      type="text"
+                      rows="5"
+                      value={formData.description}
+                      onChange={handleChange}
+                    />
+                    <label htmlFor="description" className="descriptionLabel">
+                      Description
+                    </label>
+                  </div>
+                </div>
+                <button type="submit">Submit</button>
+              </form>
+            )}
           </div>
         </Animate>
       </div>
     </section>
   );
 };
+
 export default Contact;
