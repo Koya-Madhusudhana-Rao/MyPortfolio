04ba4bca2fa8e733456139ead57eda24253f214c
Madhusudhana Rao <madhusudhanaraokoya@gmail.com>
2024-05-05 11:20:52 +0530

    Added new Features

--- src/containers/resume/utils.js
+++ src/containers/resume/utils.js
@@ -1,48 +1,39 @@
 export const data = {
-    experience: [
-      {
-        title: "AWS CLOUD PRACTITIONER",
-        subTitle: "AWS, CP",
-        description:
-          " Aws cloud practitioner basics working with AWS cloud",
-      },
-      {
-        title: "MERN FULL STACK",
-        subTitle: "MERN",
-        description:
-          "Learned MongoDB ReactJS ExpressJS and NodeJS and Done Auction Website ",
-      },
-     {
-        title: "Data Analytics ",
-        subTitle: "Certification",
-        description:
-         "Learned PowerBI and Advanced Excel Concepts and Did COVID-19 Data Analysis Tracker",
-     },
-      
-    ],
-    education: [
-      {
-        title: "Secondary",
-        subTitle: "GOOD NEWS ENGLISH MEDIUM HIGH SCHOOL",
-        description:
-          "10th Grade",
-      },
-      {
-        title: "Higher Secondary",
-        subTitle: "NARAYANA JUNIOR COLLEGE",
-        description:
-          "12th Grade in Science",
-
-        
-        
-        
-        
-      },
-      {
-        title: "B.Tech",
-        subTitle: "Vellore Institute of Technology - AP",
-        description:
-          "Bachelor of technology in computer science specializing in Artificial intelligence and Machine Learning",
-      },
-    ],
-  };
+  experience: [
+    {
+      title: "AWS CLOUD PRACTITIONER",
+      subTitle: "AWS, CP",
+      description: "Aws cloud practitioner basics working with AWS cloud",
+      certificateLink: process.env.AWS_CERTIFICATE_LINK
+    },
+    {
+      title: "MERN FULL STACK",
+      subTitle: "MERN",
+      description: "Learned MongoDB ReactJS ExpressJS and NodeJS and Done Auction Website",
+      certificateLink: process.env.MERN_CERTIFICATE_LINK
+    },
+    {
+      title: "Data Analytics",
+      subTitle: "Certification",
+      description: "Learned PowerBI and Advanced Excel Concepts and Did COVID-19 Data Analysis Tracker",
+      certificateLink: process.env.DATA_ANALYTICS_CERTIFICATE_LINK
+    },
+  ],
+  education: [
+    {
+      title: "Secondary",
+      subTitle: "GOOD NEWS ENGLISH MEDIUM HIGH SCHOOL",
+      description: "10th Grade",
+    },
+    {
+      title: "Higher Secondary",
+      subTitle: "NARAYANA JUNIOR COLLEGE",
+      description: "12th Grade in Science",
+    },
+    {
+      title: "B.Tech",
+      subTitle: "Vellore Institute of Technology - AP",
+      description: "Bachelor of technology in computer science specializing in Artificial intelligence and Machine Learning",
+    },
+  ],
+};
